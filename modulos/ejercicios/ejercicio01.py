c52838133f076d8a2006c005171736566361136c
Dario Gimenez <daseg2109@gmail.com>
2023-01-31 11:10:20 -0300

    Ej01-modulos

---
+++ modulos/ejercicios/ejercicio01.py
@@ -0,0 +1,11 @@
+import operaciones as op
+
+
+def main():
+    print(f'El resultado de la suma es: {op.sumar(2,2)}')
+    print(f'El resultado de la resta es: {op.restar(2,2)}')
+    print(f'El resultado de la multiplicación es: {op.multiplicar(2,2)}')
+    print(f'El resultado de la divición es: {op.dividir(2,2)}')
+
+if __name__ == '__main__':
+    main()
---
+++ modulos/ejercicios/operaciones.py
@@ -0,0 +1,21 @@
+""" 
+En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
+
+Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.
+"""
+
+
+def sumar(a: int, b: int):
+    return a + b
+
+
+def restar(a: int, b: int):
+    return a - b
+
+
+def multiplicar(a: int, b: int):
+    return a*b
+
+
+def dividir(a: int, b: int):
+    return a/b

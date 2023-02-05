d2f5ba531a7d8db121603b7200dba0360be57aa0
Dario Gimenez <daseg2109@gmail.com>
2023-02-05 20:03:13 -0300

    refactorizacion

--- 11-DB/main.py
+++ 11-DB/main.py
@@ -3,7 +3,7 @@ import getpass
 
 
 def main():
-    crear_usuario(5,'miguel2','gmnz')
+    crear_usuario(5, 'miguel2', 'gmnz')
 
 
 def main2():
@@ -31,20 +31,16 @@ def verificar_credenciales(username, password):
 
 
 def crear_usuario(identificador, username, password):
-    conn = sqlite3.connect("./11-DB/miaplicacion.db",isolation_level=None)
+    conn = sqlite3.connect("./11-DB/miaplicacion.db", isolation_level=None)
     cursor = conn.cursor()
 
     query = '''INSERT INTO users(id,username,password) VALUES(?,?,?)'''
 
-    rows = conn.execute(query,(identificador,username,password))
+    rows = conn.execute(query, (identificador, username, password))
 
     cursor.close()
     conn.close()
-    
-  
 
-
-    
 
 if __name__ == '__main__':
     main()
